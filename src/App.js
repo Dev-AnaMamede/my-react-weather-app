c9c08ef8d1421559fb84f9195af7d0bc4f875db2
AnaMamede-dev <AnaMamede-dev@users.noreply.github.com>
2020-08-24 04:11:52 +0100

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import "./App.css";
 export default function WeatherApp() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Matt Delac and is{" "}
-        <a
-          href="https://github.com/Dev-AnaMamede/my-react-weather-app"
-          target="_blank"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Matt Delac and is{" "}
+          <a
+            href="https://github.com/Dev-AnaMamede/my-react-weather-app"
+            target="_blank"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
